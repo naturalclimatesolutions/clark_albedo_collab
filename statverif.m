e0cca4fe8184ea34f1809e3f6f3cbd4078deaea7
Natalia <nhasler@clarku.edu>
2021-10-29 14:01:28 -0400

    Small script to verify my statistics calculation

--- statverif.m
+++ statverif.m
@@ -1,22 +1,19 @@
-% Stat Verif
+% Statistics verifications
 
-% Check a few random points to see if my statistics are correct
+% Check a few random points to see if my calculated statistics maps correspond to the same
+%   statistics perfomed on the individual kernel maps
 
 
 clearvars
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-computer = "GEO-007264";                % where the routine is run (for directory paths):
-                                        %   GEO-007264 (my laptop)
-                                        %   GEO-005199 (old supercomputer)
-                                        %   GEO-006381 (new supercomputer)
+% 1. User-defined inputs
+% ----------------------
+computer = "GEO-005199";                % where the routine is run (for directory paths):
 desiredstatistics = ...                 % list of statistics to be performed
     ["min","max","mean","median","range","standard deviation"];
-nstat = length(statistics);
-npts = 1000;
-
-% **********************
+nstat = length(desiredstatistics);
+npts = 1000;                            % number of random land points on which to verif the stats
 
 
 % 2. Define inputs
@@ -24,78 +21,77 @@ npts = 1000;
 switch computer
     case "GEO-007264"
         rootdir = 'C:\Work\GlobalAlbedo\';
-        mapdir = strcat('G:\GlobalAlbedo\');
-        resultdir = rootdir;
     otherwise
         rootdir = 'D:\NCS-GlobalAlbedo\FilledAlbedo\';
-        mapdir = strcat('G:\TNC\GlobalAlbedo\');
-        resultdir = 'E:\NCS-GlobalAlbedo\';
 end
 
 glodatafname = strcat(rootdir,"AlbedoGeneralData.mat");
-load(glodatafname,'IGBPBiomes','biomes_igbp','latlonscale','nblocks','nlat','nlon','nlcc',...
-    'nmonths','nkernels','npw','presland','pathwayslandcover','pwnames','pwid','kernels',...
-    'regoutputfiles','resultslocalfolder','landmask','blocksize','kernelscale')
+load(glodatafname,'IGBPBiomes','biomes_igbp','latlonscale','nlat','nlon','nlcc','nkernels',...
+    'pathwayslandcover','pwnames','kernels','resultslocalfolder','landmask','pwid')
 
+lat05 = 90 - latlonscale/2 : -latlonscale : -90 + latlonscale/2;
+lon05 = -180 + latlonscale/2 : latlonscale : 180 - latlonscale/2;
+[lons,lats] = meshgrid(lon05,lat05);
+latA = -60;
+Antarctica = lats <= latA;
 
+landmask(Antarctica) = false;
 [lat,lon] = find(landmask);
 
-pt = random('uniform',1,length(lat),[npts,1]);
 
-kerndata = NaN(npts,nkernels);
-for kk = 1 : nkernels
-    fname = strcat(resultslocalfolder,'Forest2Grass\EBF2GRA_',kernels(kk),'.tif');
-    val = readgeoraster(fname);
-    kerndata(:,kk) = var(lat(pt),lon(pt));
-end
+% 3. Calculate/compare statistics from geotiff and statistic maps (on some random land points)
+% ---------------------------------------------------------------
+for ll = 1 : nlcc
+    name = strcat(pathwayslandcover(ll,1),"2",pathwayslandcover(ll,2));
+    lc1 = biomes_igbp == pathwayslandcover(ll,1);
+    lc2 = biomes_igbp == pathwayslandcover(ll,2);
 
-statdata = NaN(npts,nstat);
-for ss = 1 : nstat
-    statname = replace(desiredstatistics(ss)," ","_");
-    fname = strcat(resultslocalfolder,'Forest2Grass\EBF2GRA_',statname(ss),'.tif');
-    val = readgeoraster(fname);
-    statdata(:,ss) = var(lat(pt),lon(pt));
-end
-
-
-for ss = 1 : nstat
-    stat = desiredstatistics(ss);
-    switch stat
-        case "min"
-            val = min(kerndata,[],2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-        case "max"
-            val = max(kerndata,[],2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-        case "median"
-            val = median(kerndata,2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-        case "mean"
-            val = mean(kerndata,2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-        case "standard deviation"
-            val = std(kerndata,0,2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-        case "range"
-            val = range(kerndata,2,'omitnan');
-            if isempty(val-statdata(:,ss)) == 0
-                warning(strcat("My statistics calculation don't match for ",stat));
-            end
-    end
-end
-
-
-
+    folder = pwnames(pwid(ll));
     
+    pt = random('discrete uniform',length(lat),[npts,1]);
 
+    kerndata = NaN(npts,nkernels);
+    statdata = NaN(npts,nstat);
+    
+    for kk = 1 : nkernels
+        val = readgeoraster(strcat(resultslocalfolder,folder,"\",name,"_",kernels(kk),".tif"));
+        for pp = 1 : npts
+            kerndata(pp,kk) = val(lat(pt(pp)),lon(pt(pp)));
+        end
+    end
+
+    for ss = 1 : nstat
+        statname = replace(desiredstatistics(ss)," ","_");
+        val = readgeoraster(strcat(resultslocalfolder,folder,"\",name,"_",statname,'.tif'));
+        for pp = 1 : npts
+            statdata(pp,ss) = val(lat(pt(pp)),lon(pt(pp)));
+        end
+    end
+
+    for ss = 1 : nstat
+        stat = desiredstatistics(ss);
+        msgerr = strcat("My statistics' calculations don't match for ",stat," in ",name);
+        switch stat
+            case "min"
+                val = min(kerndata,[],2,'omitnan');
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+            case "max"
+                val = max(kerndata,[],2,'omitnan');
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+            case "median"
+                val = median(kerndata,2,'omitnan');
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+            case "mean"
+                val = mean(kerndata,2,'omitnan');
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+            case "standard deviation"
+                val = std(kerndata,0,2,'omitnan');
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+            case "range"
+                val = range(kerndata,2);
+                if isempty(find(val-statdata(:,ss),1)) == 0, error(msgerr); end
+        end
+    end
+
+end
 
