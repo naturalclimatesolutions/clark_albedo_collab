27019384aed01225823082f2ca1193e9b49c80bc
Natalia <nhasler@clarku.edu>
2024-02-13 11:28:10 -0500

    Updated to reflect new maps/names etc.. for Data Availability

--- ExportSinglePathwayMaps.m
+++ ExportSinglePathwayMaps.m
@@ -1,7 +1,7 @@
 % Export GeoTIFF maps of all single land-cover change pathway albedo-change induced CO2e TOA RF
 
 % Prepare arrays
-[~,lats] = meshgrid(lon05,lat05);
+[lons,lats] = meshgrid(lon05,lat05);
 latA = -60;
 Antarctica = lats <= latA;
 emptymap = ones(nlat05,nlon05,'int16') .* i16noval;
@@ -27,19 +27,37 @@ for rr = 1 : nblocks
     for ll = 1 : nlcc
         for kk = 1 : nkernels
             kmapname = strcat(lower(pathwayslandcover(ll,1)),lower(pathwayslandcover(ll,2)),kernels(kk));
-            eval(strcat(kmapname,"(regi,regj) = int16(regco2e(:,:,kk,ll));"))
+            map = regco2e(:,:,kk,ll);
+            map(isnan(map)) = i16noval;
+            eval(strcat(kmapname,"(regi,regj) = int16(map);"))
+            clear kmapname map
         end
         for ss = 1 : nstats
             smapname = strcat(lower(pathwayslandcover(ll,1)),lower(pathwayslandcover(ll,2)),statsnames(ss));
-            eval(strcat(smapname,"(regi,regj) = int16(regco2estat(:,:,ss,ll));"))
+            map = regco2estat(:,:,ss,ll);
+            map(isnan(map)) = i16noval;
+            eval(strcat(smapname,"(regi,regj) = int16(map);"))
+            clear smapname map
         end
-        clear kmapname smapname kk ss
+        clear kk ss
     end
 
     if floor(rr/20) == rr/20
         strcat("done with building single pathway maps in subregion ",num2str(rr))
     end
 end
+
+% Save the median maps for later use
+desstat = "median";
+for ll = 1 : nlcc
+    smapname = strcat(lower(pathwayslandcover(ll,1)),lower(pathwayslandcover(ll,2)),desstat);
+    if exist(DataAvailabilityMatFile,'file') == 2
+        save(DataAvailabilityMatFile,smapname,'-append')
+    else
+        save(DataAvailabilityMatFile,smapname)
+    end
+end
+
 
 % Export GeoTIFFs
 for ll = 1 : nlcc
@@ -74,6 +92,24 @@ for ll = 1 : nlcc
     end
     strcat("done with exporting geotiff maps of ",arootname)
     clear pp pname fold fold2 arootname grootname kk ss
+end
+
+
+% Print figure if desired
+thisfiguretype = "one panels";
+axislegend = 'Mg CO_2e ha^-^1';
+
+for ll = 1 : nlcc
+    for ss = 1 : nstats
+        smapname = strcat(lower(pathwayslandcover(ll,1)),lower(pathwayslandcover(ll,2)),statsnames(ss));
+        eval(strcat("data =",smapname,";"))
+        figurename = strcat(figuredir,pathwayslandcover(ll,1),"2",pathwayslandcover(ll,2),"_",statsnames(ss));
+        thiscolorbar = co2colorbar;
+        thesecategories = seqcat;
+        figure(ll); clf
+        h = albedofigure(data,seqcat,lats,lons,figurename,thisfiguretype,co2colorbar,...
+            [],coastlat,coastlon,axislegend,false);
+    end
 end
 
 
