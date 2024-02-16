1ae61134c343e5cc605524686e59b79677959fe3
Natalia Hasler <NHasler@clarku.edu>
2024-02-16 11:33:56 -0500

    Renamed figure folder

--- Figure2_2DHistograms/reproject_rasters_to_equal_area.sh
+++
@@ -1,5 +0,0 @@
-#NOTE: needs to be run in the directory containing the net climate impact, refor albedo radiative forcing, and rasterized biome GeoTIFFs
-for rast in `ls *tif`
-do
-   gdalwarp -t_srs ESRI:54012 -srcnodata 255 -dstnodata 255 $rast "${rast/%.tif/_ECKERTIV.tif}"
-done
